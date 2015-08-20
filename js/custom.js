a7bb8ffd7544de0a0883423c551d3135e800cd3f
Joel Majors <jam.majors@icloud.com>
2015-08-20 16:19:17 -0400

    Added links to the SVG for region sites. Simple edit on Asia.html commented out original javascript for SVGs

--- js/custom.js
+++ js/custom.js
@@ -1,20 +1,24 @@
 // SVGMap
 // ====================
-    AmCharts.ready(function() {
-        var map = new AmCharts.AmMap();
-        map.pathToImages = "ammap/images/";
-        var dataProvider = {
-            map: "continentsLow",
-            getAreasFromMap:true                    
-        }; 
-        // pass data provider to the map object
-        map.dataProvider = dataProvider;
+    // AmCharts.ready(function() {
+    //     var map = new AmCharts.AmMap();
+    //     map.pathToImages = "ammap/images/";
+    //     var dataProvider = {
+    //         map: "continentsLow",
+    //         getAreasFromMap:true                    
+    //     }; 
+    //     // pass data provider to the map object
+    //     map.dataProvider = dataProvider;
 
-        map.areasSettings = {
-            autoZoom: true,
-            selectedColor: "#CC0000"
-        };
+    //     map.areasSettings = {
+    //         autoZoom: true,
+    //         selectedColor: "#CC0000"
+    //     };
 
-        // write the map to container div
-        map.write("mapdiv");
-    });
+    //     // write the map to container div
+    //     map.write("mapdiv");
+    // });
+
+$("#north_america.land").click(function() {
+    
+});
