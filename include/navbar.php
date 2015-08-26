cc8670ab2f26d838f5e82dff2a09924c7867df40
Joel Majors <jam.majors@icloud.com>
2015-08-25 20:54:07 -0400

    fixed link to main page in navbar

--- include/navbar.php
+++ include/navbar.php
@@ -1,6 +1,6 @@
 	<div class="navbar navbar-default navbar-fixed-top">
 		<div class="container">
-			<a class="navbar-text logo-text pull-left" href="index.html">missionsLouisville</a>
+			<a class="navbar-text logo-text pull-left" href="/">missionsLouisville</a>
 			<p class="navbar-text pull-right hidden-xs hidden-xxs">from the Ville to the World</p>
 		</div>
 	</div>
