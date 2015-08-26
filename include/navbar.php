0008cda6455263cc52c2aa24f70550cd36c7a8ad
Joel Majors <jam.majors@icloud.com>
2015-08-25 20:19:09 -0400

    switched to php for navbar and footer.

---
+++ include/navbar.php
@@ -0,0 +1,6 @@
+	<div class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<a class="navbar-text logo-text pull-left" href="index.html">missionsLouisville</a>
+			<p class="navbar-text pull-right hidden-xs hidden-xxs">from the Ville to the World</p>
+		</div>
+	</div>
