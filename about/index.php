211bb6c242ca2db9093f37972c5de4df5a60c1c1
Joel Majors <jam.majors@icloud.com>
2015-08-26 18:07:44 -0400

    adjusted About page. paragraphs

--- about/index.php
+++ about/index.php
@@ -10,9 +10,9 @@
 			<h1>Here is the About page</h1>
 			<p>
 				missionsLouisville is designed to bring together brothers and sisters in Louisville, Kentucky around the topic and work of missions. Find out and join in on what other churches are doing here and around the world. 
-				<br>
-				Keep scrolling to find some great resources. (Later, that is. I'll add some books that you should check out. Aside from "The Good Book", of course.)
-				<br>		
+				</p>
+				<p>Keep scrolling to find some great resources. (Later, that is. I'll add some books that you should check out. Aside from "The Good Book", of course.)
+				
 			</p>
 		</div>
 	</div>
