6c64c4e88aaa4134a23abddab2282f00b7f6934e
Joel Majors <jam.majors@icloud.com>
2015-08-25 20:59:02 -0400

    fixing links, making "about" php compliant

---
+++ about/index.php
@@ -0,0 +1,20 @@
+<?php
+
+	$title = "missionsLouisville";
+
+	include('/var/www/missionslouisville.org/public_html/include/header.php'); ?>
+	
+	<!-- Jumbotron -->
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Here is the About page</h1>
+			<p>
+				missionsLouisville is designed to bring together brothers and sisters in Louisville, Kentucky around the topic and work of missions. Find out and join in on what other churches are doing here and around the world. 
+				<br>
+				Keep scrolling to find some great resources.
+			</p>
+		</div>
+	</div>
+	<!-- End Jumbotron -->
+<!-- Footer -->
+<? include("/var/www/missionslouisville.org/public_html/include/footer.php"); ?>
