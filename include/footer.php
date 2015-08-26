2e0c5cd75dd5f5fbe2d1922d987ba54c223ca573
Joel Majors <jam.majors@icloud.com>
2015-08-26 13:07:27 -0400

    maybe the size checker won't jack up the footer now.

--- include/footer.php
+++ include/footer.php
@@ -1,7 +1,10 @@
-<div class="container footer-container">
-<div class="row">
-	<? include("/var/www/missionslouisville.org/public_html/include/size-checker.php"); ?>
+<div class="container-flow">
+	<div class="row">
+		<? include("/var/www/missionslouisville.org/public_html/include/size-checker.php"); ?>
+	</div>
 </div>
+
+<div class="container footer-container">
 		<div class="row">
 			<div class="col-xs-6 col-sm-4">
 				<p>The riddle of the sphynx. What creature walks on four legs in the morning, two and noon, and three at twilight?
