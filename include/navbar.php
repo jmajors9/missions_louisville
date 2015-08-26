868a14b3db308d0ca781113a2f821203e46cd1d3
Joel Majors <jam.majors@icloud.com>
2015-08-26 13:25:54 -0400

    I think I fixed the size checker

--- include/navbar.php
+++ include/navbar.php
@@ -1,7 +1,7 @@
-	<div class="navbar navbar-default navbar-fixed-top">
-		<div class="container">
-			<a class="navbar-text logo-text pull-left" href="/">missionsLouisville</a><div class="container-flow">	
-		<? include("/var/www/missionslouisville.org/public_html/include/size-checker.php"); ?>	
-			<p class="navbar-text pull-right hidden-xs hidden-xxs">from the Ville to the World</p>
-		</div>
+<div class="navbar navbar-default navbar-fixed-top">
+	<div class="container">
+		<a class="navbar-text logo-text pull-left" href="/">missionsLouisville</a>
+		<? include("/var/www/missionslouisville.org/public_html/include/size-checker.php"); ?>
+		<p class="navbar-text pull-right hidden-xs hidden-xxs">from the Ville to the World</p>
 	</div>
+</div>
