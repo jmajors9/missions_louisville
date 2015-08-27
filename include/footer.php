056f76c759c212ed8b6617c91a472f9fe1c5ad31
Joel Majors <jam.majors@icloud.com>
2015-08-27 18:45:37 -0400

    fixed it all!! footer forever!!

--- include/footer.php
+++ include/footer.php
@@ -1,14 +1,14 @@
 		<footer class="footer-top">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-5 col-sm-4">
+					<div class="col-xs-6 col-sm-4">
 						<p class="text-center">The riddle of the sphynx. What creature walks on four legs in the morning, two and noon, and three at twilight?
 						</p>
 					</div>
-					<div class="col-xs-2 col-sm-4 hidden-xxs">
-						<img class="footer-fleur-top img-responsive" src="/img/Fleur-de-lis-fill.svg">				
+					<div class="col-sm-4 hidden-xxs">
+						<img class="footer-fleur-top center-block" src="/img/Fleur-de-lis-fill.svg">				
 					</div>
-					<div class="col-xs-5 col-sm-4">
+					<div class="col-xs-6 col-sm-4">
 						<p class="text-center">Do you have an Event or Person to add?
 						</p>
 						<a class="btn btn-default btn-block" href="/add-event/" role="button">Add Event/Person</a>
@@ -17,8 +17,8 @@
 			</div>
 		</footer>
 		<footer class="footer footer-bottom panel-footer">      
-      		<div class="visible-xxs">
-				<img class="footer-fleur-bottom img-responsive" src="/img/Fleur-de-lis-fill.svg">
+      		<div class="visible-xs visible-xxs">
+				<img class="footer-fleur-bottom center-block" src="/img/Fleur-de-lis-fill.svg">
 			</div>
 	      	<div class="text-center">
 	      		<p>Copyright &copy; 2015 - TheVille || A MajorStudios Website</p>
