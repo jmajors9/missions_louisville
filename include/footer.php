7fe716170272ac7892314efb8769589a8cc10386
Joel Majors <jam.majors@icloud.com>
2015-08-27 18:47:13 -0400

    dang. maybe I have it now.

--- include/footer.php
+++ include/footer.php
@@ -5,7 +5,7 @@
 						<p class="text-center">The riddle of the sphynx. What creature walks on four legs in the morning, two and noon, and three at twilight?
 						</p>
 					</div>
-					<div class="col-sm-4 hidden-xxs">
+					<div class="col-sm-4 hidden-xs hidden-xxs">
 						<img class="footer-fleur-top center-block" src="/img/Fleur-de-lis-fill.svg">				
 					</div>
 					<div class="col-xs-6 col-sm-4">
