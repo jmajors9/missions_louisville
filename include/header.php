4c4e7609e5d64128825880e0125708f3b768d206
Joel Majors <jam.majors@icloud.com>
2015-08-28 14:46:47 -0400

    trying out a new font.

--- include/header.php
+++ include/header.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<title><?=$title?></title>
 		<meta name="viewport" content="width=device-width">
+		<link href='https://fonts.googleapis.com/css?family=News+Cycle|Quattrocento+Sans:400,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" type="text/css" href="/css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="/css/custom-styles.css">
 	  	<script src="https://maps.googleapis.com/maps/api/js"></script>	  	
