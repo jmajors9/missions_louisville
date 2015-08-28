b8a22f200dcf8a889245ac243fbf966b6ebcdc03
Joel Majors <jam.majors@icloud.com>
2015-08-28 15:29:59 -0400

    again

--- include/navbar.php
+++ include/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-default navbar-collapse">
 	<div class="container-fluid">
-		<div class="navbar-header">
+		<div class="navbar-header navbar-text">
 			<!-- <a class="navbar-text navbar-left visible-xxs" href="/">missionsLouisville XXS</a>
 			<a class="navbar-text navbar-left visible-xs" href="/">missionsLouisville XS</a>
 			<a class="navbar-text navbar-left visible-sm" href="/">missionsLouisville SM</a>
 			<a class="navbar-text navbar-left visible-md" href="/">missionsLouisville MD</a>
 			<a class="navbar-text navbar-left visible-lg" href="/">missionsLouisville LG</a> -->
-			<a class="navbar-text navbar-left" href="/">missionsLouisville</a>
+			<a class="navbar-left" href="/">missionsLouisville</a>
 		</div>
 		<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>	
 	</div>
