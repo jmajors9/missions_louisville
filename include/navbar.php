ff03e8122166a1c9a5d1c48d1a32924acefec5b1
Joel Majors <jam.majors@icloud.com>
2015-08-28 15:04:36 -0400

    more navbar

--- include/navbar.php
+++ include/navbar.php
@@ -6,7 +6,9 @@
 			<a class="navbar-text navbar-left visible-sm" href="/">missionsLouisville SM</a>
 			<a class="navbar-text navbar-left visible-md" href="/">missionsLouisville MD</a>
 			<a class="navbar-text navbar-left visible-lg" href="/">missionsLouisville LG</a>
-			</div>
-			<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>	
+		</div>
+		<div class="navbar-header">
+			<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>
+		</div>
 	</div>
 </nav>
