caa97ce64507b1a43d5c8604a18ff384e9f66092
Joel Majors <jam.majors@icloud.com>
2015-08-28 15:08:01 -0400

    fooling with fonts.

--- include/navbar.php
+++ include/navbar.php
@@ -1,14 +1,13 @@
 <nav class="navbar navbar-default navbar-collapse">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a class="navbar-text navbar-left visible-xxs" href="/">missionsLouisville XXS</a>
+			<!-- <a class="navbar-text navbar-left visible-xxs" href="/">missionsLouisville XXS</a>
 			<a class="navbar-text navbar-left visible-xs" href="/">missionsLouisville XS</a>
 			<a class="navbar-text navbar-left visible-sm" href="/">missionsLouisville SM</a>
 			<a class="navbar-text navbar-left visible-md" href="/">missionsLouisville MD</a>
-			<a class="navbar-text navbar-left visible-lg" href="/">missionsLouisville LG</a>
+			<a class="navbar-text navbar-left visible-lg" href="/">missionsLouisville LG</a> -->
+			<a class="navbar-text navbar-left" href="/">missionsLouisville</a>
 		</div>
-		<div class="navbar-header">
-			<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>
-		</div>
+		<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>	
 	</div>
 </nav>
