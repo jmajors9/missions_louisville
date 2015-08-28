4fd9dfdc974fb5cfcf129df554d564908455cd82
Joel Majors <jam.majors@icloud.com>
2015-08-28 15:02:14 -0400

    testy righty

--- include/navbar.php
+++ include/navbar.php
@@ -6,8 +6,7 @@
 			<a class="navbar-text navbar-left visible-sm" href="/">missionsLouisville SM</a>
 			<a class="navbar-text navbar-left visible-md" href="/">missionsLouisville MD</a>
 			<a class="navbar-text navbar-left visible-lg" href="/">missionsLouisville LG</a>
-			<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>
-			<p class="navbar-text navbar-right">Signed in as <a href="#" class="navbar-link">Mark Otto</a></p>
-		</div>
+			</div>
+			<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>	
 	</div>
 </nav>
