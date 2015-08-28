926437c152018a1a0e7b72f3b1afe07b357e8944
Joel Majors <jam.majors@icloud.com>
2015-08-28 12:30:13 -0400

    a tiny change to the footer to help differentiate at smaller sizes

--- include/footer.php
+++ include/footer.php
@@ -1,16 +1,14 @@
 		<footer class="footer-top">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-6 col-sm-4">
-						<p class="text-center">The riddle of the sphynx. What creature walks on four legs in the morning, two and noon, and three at twilight?
-						</p>
+					<div class="left-footer col-xs-6 col-sm-4 ">
+						<p class="text-center">Thank you for visiting our site. If your church is not listed, tell your pastor or click "Add Event" to the right!</p>
 					</div>
 					<div class="col-sm-4 hidden-xs hidden-xxs">
 						<img class="footer-fleur-top center-block" src="/img/Fleur-de-lis-fill.svg">				
 					</div>
 					<div class="col-xs-6 col-sm-4">
-						<p class="text-center">Do you have an Event or Person to add?
-						</p>
+						<p class="text-center">Do you have an Event or Person to add?</p>
 						<a class="btn btn-default btn-block" href="/add-event/" role="button">Add Event/Person</a>
 					</div>
 				</div>
