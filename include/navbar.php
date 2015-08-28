4b5c833a817c4e30b5ba2055614d8fc5d8a1a781
Joel Majors <jam.majors@icloud.com>
2015-08-28 14:56:54 -0400

    navbar fixings

--- include/navbar.php
+++ include/navbar.php
@@ -1,12 +1,12 @@
 <nav class="navbar navbar-default navbar-collapse">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a class="navbar-text logo-text pull-left visible-xxs" href="/">missionsLouisville XXS</a>
-			<a class="navbar-text logo-text pull-left visible-xs" href="/">missionsLouisville XS</a>
-			<a class="navbar-text logo-text pull-left visible-sm" href="/">missionsLouisville SM</a>
-			<a class="navbar-text logo-text pull-left visible-md" href="/">missionsLouisville MD</a>
-			<a class="navbar-text logo-text pull-left visible-lg" href="/">missionsLouisville LG</a>
-			<p class="navbar-text pull-right hidden-xs hidden-xxs">from the Ville to the World</p>
+			<a class="navbar-text logo-text navbar-left visible-xxs" href="/">missionsLouisville XXS</a>
+			<a class="navbar-text logo-text navbar-left visible-xs" href="/">missionsLouisville XS</a>
+			<a class="navbar-text logo-text navbar-left visible-sm" href="/">missionsLouisville SM</a>
+			<a class="navbar-text logo-text navbar-left visible-md" href="/">missionsLouisville MD</a>
+			<a class="navbar-text logo-text navbar-left visible-lg" href="/">missionsLouisville LG</a>
+			<p class="navbar-text navbar-right hidden-xs hidden-xxs">from the Ville to the World</p>
 		</div>
 	</div>
 </nav>
