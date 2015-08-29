e40f04709778e3b9cc10c5ade49dc6566c4094bb
Joel Majors <jam.majors@icloud.com>
2015-08-29 15:46:45 -0400

    more testing

--- js/custom.js
+++ js/custom.js
@@ -18,27 +18,60 @@
 //   }
 // }
 
+// My playing around
+
+// var App = {
+//   attachHandlers: function(){
+//     $('path').click(function(){
+//       var countryID = $(this).attr('title');
+//       $.each(App.data.events, function(i, event){
+//         if(event.path == countryID){
+//           console.log(event);
+//         } else {
+//           console.log("Sumpn ain't raht")
+//         }
+//       });
+//     });
+//   },
+//   data: '',
+//   fetch: function(){
+//     $.getJSON('/data.json', function(response){
+//       App.data = response;
+//     });
+//   },
+//   start: function(){
+//     App.fetch();
+//     App.attachHandlers();
+//   }
+// }
+
+
+// Steven's original
 var App = {
-  attachHandlers: function(){
-    $('path').click(function(){
-      var countryID = $(this).attr('title');
-      $.each(App.data.events, function(i, event){
-        if(event.path == countryID){
-          console.log(event);
-        } else {
-          console.log("Sumpn ain't raht")
-        }
-      });
-    });
-  },
-  data: '',
-  fetch: function(){
-    $.getJSON('/data.json', function(response){
-      App.data = response;
-    });
-  },
-  start: function(){
-    App.fetch();
-    App.attachHandlers();
-  }
+    attachHandlers: function(){
+
+        $('.country').click(function(){
+
+            var countryID = $(this).attr('id');
+
+            $.each(App.data.events, function(i, event){
+
+                if(event.country == countryID){
+
+                    console.log(event);
+                }
+            });
+        });
+    },
+    data: '',
+    fetch: function(){
+        $.getJSON('url', function(response){
+
+            App.data = response;
+        });
+    },
+    start: function(){
+        App.fetch();
+        App.attachHandlers();
+    }
 }
