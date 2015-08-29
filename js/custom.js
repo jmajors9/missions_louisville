e69a374f99368032d5ce212791f98a9a4b3f9ecd
Joel Majors <jam.majors@icloud.com>
2015-08-29 12:01:24 -0400

    always fixing the footer...

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 var App = {
-  // attachHandlers: function(){
-  //   $("path").click(function(e){
-  //     e.preventDefault();
-  //     console.log($(this).attr("title"));
+  attachHandlers: function(){
+    $("path").click(function(e){
+      e.preventDefault();
+      console.log($(this).attr("title"));
 
-  //   });
-  // },
+    });
+  },
   data: '',
   fetch: function () {
     $.getJSON("./data.json", function (response) {
