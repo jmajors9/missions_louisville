640067dcca35e49519f57c3e3933fe48a9a3c134
Joel Majors <jam.majors@icloud.com>
2015-08-29 15:44:59 -0400

    still trying to make json bend to my will

--- js/custom.js
+++ js/custom.js
@@ -19,29 +19,26 @@
 // }
 
 var App = {
-    attachHandlers: function(){
-        $('path').click(function(){
-            var countryID = $(this).attr('title');
-            $.each(App.data.events, function(i, event){
-
-                if(event.path == countryID){
-
-                    console.log(event);
-                } else {
-                  console.log("Sumpn ain't raht")
-                }
-            });
-        });
-    },
-    data: '',
-    fetch: function(){
-        $.getJSON('/data.json', function(response){
-
-            App.data = response;
-        });
-    },
-    start: function(){
-        App.fetch();
-        App.attachHandlers();
-    }
+  attachHandlers: function(){
+    $('path').click(function(){
+      var countryID = $(this).attr('title');
+      $.each(App.data.events, function(i, event){
+        if(event.path == countryID){
+          console.log(event);
+        } else {
+          console.log("Sumpn ain't raht")
+        }
+      });
+    });
+  },
+  data: '',
+  fetch: function(){
+    $.getJSON('/data.json', function(response){
+      App.data = response;
+    });
+  },
+  start: function(){
+    App.fetch();
+    App.attachHandlers();
+  }
 }
