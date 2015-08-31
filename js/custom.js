09c02ae289c33a54627d92bf1010851caa37522c
Joel Majors <jam.majors@icloud.com>
2015-08-30 21:14:06 -0400

    oops.

--- js/custom.js
+++ js/custom.js
@@ -17,14 +17,6 @@ var App = {
       console.log(response);
     });
   },
-  reverseDisplay: function(d) {
-    if (document.getElementById(d).style.display == "none") {
-      document.getElementById(d).style.display = "block";
-    }
-    else {
-      document.getElementById(d).style.display = "none";
-    }
-  },
   start: function(){
     App.fetch();
     App.attachHandlers();
@@ -33,18 +25,11 @@ var App = {
 };
 
 //show or hide event or missions on about page.
-
-// function HideContent(d) {
-// document.getElementById(d).style.display = "none";
-// }
-// function ShowContent(d) {
-// document.getElementById(d).style.display = "block";
-// }
-// function ReverseDisplay(d) {
-// if (document.getElementById(d).style.display == "none") {
-//     document.getElementById(d).style.display = "block";
-//   }
-// else {
-//   document.getElementById(d).style.display = "none";
-//   }
-// }
+function ReverseDisplay(d) {
+if (document.getElementById(d).style.display == "none") {
+    document.getElementById(d).style.display = "block";
+  }
+else {
+  document.getElementById(d).style.display = "none";
+  }
+}
