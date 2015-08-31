c2063468708f31dcfd31cde0224a1c08a462715d
Joel Majors <jam.majors@icloud.com>
2015-08-30 22:20:28 -0400

    AGAIN!

--- africa/index.php
+++ africa/index.php
@@ -99,10 +99,10 @@
 			<div class="panel panel-default">
 				<h3 class="panel-heading">Burkina Faso</h3>
 				<h4 class="panel-body">
-					Church:<small> Shelby Park Presbyterian</small>
-					Departure Date:<small> July 1, 2016</small>
-					Return Date:<small> July 20, 2016</small>
-					Description:<small> Each year we send a group to work a camp for orphaned children.</small>
+					<p>Church:<small> Shelby Park Presbyterian</small></p>
+					<p>Departure Date:<small> July 1, 2016</small></p>
+					<p>Return Date:<small> July 20, 2016</small></p>
+					<p>Description:<small> Each year we send a group to work a camp for orphaned children.</small></p>
 				</h4>
 			</div>	
 		</div>	
