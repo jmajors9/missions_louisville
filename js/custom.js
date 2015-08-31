154a802305a9abf44e82b0ba76d2c40c76bfd9fb
Joel Majors <jam.majors@icloud.com>
2015-08-30 21:06:59 -0400

    again

--- js/custom.js
+++ js/custom.js
@@ -34,17 +34,17 @@ var App = {
 
 //show or hide event or missions on about page.
 
-function HideContent(d) {
-document.getElementById(d).style.display = "none";
-}
-function ShowContent(d) {
-document.getElementById(d).style.display = "block";
-}
-function ReverseDisplay(d) {
-if (document.getElementById(d).style.display == "none") {
-    document.getElementById(d).style.display = "block";
-  }
-else {
-  document.getElementById(d).style.display = "none";
-  }
-}
+// function HideContent(d) {
+// document.getElementById(d).style.display = "none";
+// }
+// function ShowContent(d) {
+// document.getElementById(d).style.display = "block";
+// }
+// function ReverseDisplay(d) {
+// if (document.getElementById(d).style.display == "none") {
+//     document.getElementById(d).style.display = "block";
+//   }
+// else {
+//   document.getElementById(d).style.display = "none";
+//   }
+// }
