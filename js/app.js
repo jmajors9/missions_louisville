ccfbfc506044898526a788ec77ca1f62084290c0
Joel Majors <jam.majors@icloud.com>
2015-09-07 17:06:12 -0400

    messin with stuff to hopefully enact AngularJS later. Good tests coming together!

---
+++ js/app.js
@@ -0,0 +1,27 @@
+(function (){
+	var app = angular.module('region', []);
+
+	app.controller('RegionEvents', function () {
+		this.event = events;
+	});
+
+	var events = {
+		id: 1,
+        longterm: false,
+        name: "engage",
+        country: "Albania",
+        email: "",
+        address: "",
+        city: "",
+        state: "",
+        zipcode: "",
+        website: "",
+        start: "20160615",
+        end: "20160815",
+        annual: true,
+        description: "Come join us as we engage the people of Albania.",
+        sender: 1,
+        churches: 1
+    
+    };
+})();
