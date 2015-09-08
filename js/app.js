30f0c4cefef5cdc5b246b340ff66cc60c2f60d1a
Joel Majors <jam.majors@icloud.com>
2015-09-08 17:32:06 -0400

    testing angular on africaTESTOR

--- js/app.js
+++ js/app.js
@@ -6,22 +6,34 @@
 	});
 
 	var events = {
-		id: 1,
-        longterm: false,
-        name: "engage",
-        country: "Albania",
-        email: "",
-        address: "",
-        city: "",
-        state: "",
-        zipcode: "",
-        website: "",
-        start: 20160615,
-        end: 20160815,
-        annual: true,
-        description: "Come join us as we engage the people of Albania.",
-        sender: 1,
-        churches: 1
-    
+            id: 1,
+            longterm: false,
+            name: "engage",
+            country: "Albania",
+            email: "",
+            address: "",
+            city: "",
+            state: "",
+            zipcode: "",
+            website: "",
+            start: 20160615,
+            end: 20160815,
+            annual: true,
+            description: "Come join us as we engage the people of Albania.",
+            sender: 1,
+            churches: 1
+
     };
+
+    app.directive("reachingWorld", function(){
+        return {
+            restrict: 'E',
+            template: '<h1>Reaching the World</h1>'
+        };
+    });
+    app.directive("manyChurches", function(){
+        return {
+            templateUrl: 'many-churches.html'
+        };
+    });
 })();
