059eef917a7a4b70aa4ed83507eff42d731b0fee
Joel Majors <jam.majors@icloud.com>
2015-09-09 22:06:31 -0400

    angular stuff

--- js/app.js
+++ js/app.js
@@ -5,7 +5,26 @@
 		this.event = events;
 	});
 
-	var events = {
+    app.controller('PanelController', function(){
+        this.tab = 1;
+        this.selectTab = function(setTab) {
+            this.tab = setTab;
+        };
+        this.isSelected = function(checkTab) {
+            return this.tab === checkTab;
+        };
+    });
+
+    app.controller('churchEvents', function () {
+        this.church = churches;
+    });
+
+    app.controller('sendingOrganization', function () {
+        this.sender = sendingOrg;
+    });
+
+
+    var events = {
             id: 1,
             longterm: false,
             name: "engage",
@@ -16,14 +35,36 @@
             state: "",
             zipcode: "",
             website: "",
-            start: 20160615,
-            end: 20160815,
+            start: "June 15, 2015",
+            end: "August 15, 2015",
             annual: true,
             description: "Come join us as we engage the people of Albania.",
             sender: 1,
             churches: 1
+        };
 
-    };
+    var churches = {
+            id: 1,
+            name: "Primero Iglesia del Louisville",
+            minister: "Julio Diaz",
+            email: "jdiaz@example.org",
+            address: "123 Any Street",
+            city: "Louisville",
+            state: "Kentucky",
+            zipcode: 40214,
+            website: "primeroiglesia.example.org"
+        };
+
+    var sendingOrg = {
+            id: 1,
+            name: "SuperNational Workers Board",
+            email: "director@example.com",
+            address: "1 This Place",
+            city: "",
+            state: "",
+            zipcode: 0,
+            website: "snwb.example.com"
+        };
 
     app.directive("reachingWorld", function(){
         return {
