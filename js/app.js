de27daf55e17b426c2c163c19281e99ae7c37364
Joel Majors <jam.majors@icloud.com>
2015-10-07 14:26:40 -0400

    oh boy

--- js/app.js
+++ js/app.js
@@ -44,94 +44,94 @@
         
     // });
 
-    var missions = {
-        "journeys":
-        [
-            {
-            "id": 1,
-            "longterm": false,
-            "name": "engage",
-            "country": "Albania",
-            "email": "",
-            "address": "",
-            "city": "",
-            "state": "",
-            "zipcode": "",
-            "website": "",
-            "start": "June 15, 2015",
-            "end": "August 15, 2015",
-            "annual": true,
-            "description": "Come join us as we engage the people of Albania.",
-            "sendingOrg": 0,
-            "church": 0
-            },
-            {
-            "id": 2,
-            "long-term": false,
-            "name": "ESL in East Asia",
-            "country": "East Asia",
-            "email": "",
-            "address": "",
-            "city": "",
-            "state": "",
-            "zipcode": "",
-            "website": "",
-            "start": "20160615",
-            "end": "20160815",
-            "annual": true,
-            "description": "Come join us as we teach English as a Second Language to the people of East Asia.",
-            "sendingOrg": 1,
-            "church": 1
-            }
-        ],
-        "church":
-        [
-            {
-                "id": 1,
-                "name": "Primero Iglesia del Louisville",
-                "minister": "Julio Diaz",
-                "email": "jdiaz@example.org",
-                "address": "123 Any Street",
-                "city": "Louisville",
-                "state": "Kentucky",
-                "zipcode": "40214",
-                "website": "primeroiglesia.example.org"
-            },
-            {
-                "id": 2,
-                "name": "View of the Valley Baptist Church",
-                "minister": "Jamal Warner",
-                "email": "jamal@example.com",
-                "address": "123 Other Avenue",
-                "city": "New Albany",
-                "state": "Indiana",
-                "zipcode": "40202",
-                "website": "vvbc.example.org"
-            }
-        ],
-        "sendingOrg":
-        [
-            {
-                "id": 1,
-                "name": "SuperNational Workers Board",
-                "email": "director@example.com",
-                "address": "1 This Place",
-                "city": "",
-                "state": "",
-                "zipcode": 0,
-                "website": "snwb.example.com"
-            },
-            {
-                "id": 2,
-                "name": "Board of North American Ministers",
-                "email": "director@example.com",
-                "address": "",
-                "city": "",
-                "state": "",
-                "zipcode": "00000",
-                "website": "bnam.example.com"
-            }
-        ]
-    };
+//     var missions = {
+//         "journeys":
+//         [
+//             {
+//             "id": 1,
+//             "longterm": false,
+//             "name": "engage",
+//             "country": "Albania",
+//             "email": "",
+//             "address": "",
+//             "city": "",
+//             "state": "",
+//             "zipcode": "",
+//             "website": "",
+//             "start": "June 15, 2015",
+//             "end": "August 15, 2015",
+//             "annual": true,
+//             "description": "Come join us as we engage the people of Albania.",
+//             "sendingOrg": 0,
+//             "church": 0
+//             },
+//             {
+//             "id": 2,
+//             "long-term": false,
+//             "name": "ESL in East Asia",
+//             "country": "East Asia",
+//             "email": "",
+//             "address": "",
+//             "city": "",
+//             "state": "",
+//             "zipcode": "",
+//             "website": "",
+//             "start": "20160615",
+//             "end": "20160815",
+//             "annual": true,
+//             "description": "Come join us as we teach English as a Second Language to the people of East Asia.",
+//             "sendingOrg": 1,
+//             "church": 1
+//             }
+//         ],
+//         "church":
+//         [
+//             {
+//                 "id": 1,
+//                 "name": "Primero Iglesia del Louisville",
+//                 "minister": "Julio Diaz",
+//                 "email": "jdiaz@example.org",
+//                 "address": "123 Any Street",
+//                 "city": "Louisville",
+//                 "state": "Kentucky",
+//                 "zipcode": "40214",
+//                 "website": "primeroiglesia.example.org"
+//             },
+//             {
+//                 "id": 2,
+//                 "name": "View of the Valley Baptist Church",
+//                 "minister": "Jamal Warner",
+//                 "email": "jamal@example.com",
+//                 "address": "123 Other Avenue",
+//                 "city": "New Albany",
+//                 "state": "Indiana",
+//                 "zipcode": "40202",
+//                 "website": "vvbc.example.org"
+//             }
+//         ],
+//         "sendingOrg":
+//         [
+//             {
+//                 "id": 1,
+//                 "name": "SuperNational Workers Board",
+//                 "email": "director@example.com",
+//                 "address": "1 This Place",
+//                 "city": "",
+//                 "state": "",
+//                 "zipcode": 0,
+//                 "website": "snwb.example.com"
+//             },
+//             {
+//                 "id": 2,
+//                 "name": "Board of North American Ministers",
+//                 "email": "director@example.com",
+//                 "address": "",
+//                 "city": "",
+//                 "state": "",
+//                 "zipcode": "00000",
+//                 "website": "bnam.example.com"
+//             }
+//         ]
+//     };
 
 })();
